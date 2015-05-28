24c75bc84425e4ca8e397980986b39e5d1035734
maxmlr <maximilian.miller@web.de>
2015-05-28 22:35:09 +0200

    Update MainActivity.java

--- app/src/main/java/de/maximilian_miller/www/frontdoor/app/MainActivity.java
+++ app/src/main/java/de/maximilian_miller/www/frontdoor/app/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+/**
+ * Main Activity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
