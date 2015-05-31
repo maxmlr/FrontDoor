8b1a32944ec96faea83ba5c609486200c3a58120
Maximilian Miller <maximilian.miller@web.de>
2015-05-31 12:38:29 +0200

    added config file for sqlite database new: codes can be valid until user defined timestamp

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$CONFIG = array (
+  'door_name'		=>	'MY_DOOR',
+  'db'			=>	'my_door.sqlite',
+  'send_command'	=>	'send 1 011100001000',
+  'send_notification'	=>	true,
+  'recipient'		=>	'my.email@gmail.com',
+  'email_from'		=>	'my.email_from@gmail.com'                                        
+);
+?>
